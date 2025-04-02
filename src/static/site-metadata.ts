5b7aa6798db270476d81c844c6d5e2580830d7ed
叫我彭一凡 <wipphj@qq.com>
2025-04-02 08:52:41 +0800

    Update site-metadata.ts

--- src/static/site-metadata.ts
+++ src/static/site-metadata.ts
@@ -16,10 +16,6 @@ const data: ISiteMetadataResult = {
   description: 'Personal site and blog',
   navLinks: [
     {
-      name: 'Blog',
-      url: 'https://github.com/yihong0618/gitblog',
-    },
-    {
       name: 'About',
       url: 'https://github.com/yihong0618/running_page/blob/master/README-CN.md',
     },
