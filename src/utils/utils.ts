f85ac0c255c9a7b65af33b28fa041c65fbb3b89e
叫我彭一凡 <wipphj@qq.com>
2025-04-02 09:48:41 +0800

    Update utils.ts

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -281,24 +281,24 @@ const titleForRun = (run: Activity): string => {
   const runDistance = run.distance / 1000;
   const runHour = +run.start_date_local.slice(11, 13);
   if (runDistance > 20 && runDistance < 40) {
-    return RUN_TITLES.HALF_MARATHON_RUN_TITLE;
+    return `${run.name.includes('徒步') ? '徒步-' : ''}${RUN_TITLES.HALF_MARATHON_RUN_TITLE}`;
   }
   if (runDistance >= 40) {
-    return RUN_TITLES.FULL_MARATHON_RUN_TITLE;
+    return `${run.name.includes('徒步') ? '徒步-' : ''}${RUN_TITLES.FULL_MARATHON_RUN_TITLE}`;
   }
   if (runHour >= 0 && runHour <= 10) {
-    return RUN_TITLES.MORNING_RUN_TITLE;
+    return `${run.name.includes('徒步') ? '清晨徒步' : RUN_TITLES.MORNING_RUN_TITLE}`;
   }
   if (runHour > 10 && runHour <= 14) {
-    return RUN_TITLES.MIDDAY_RUN_TITLE;
+    return `${run.name.includes('徒步') ? '午间徒步' : RUN_TITLES.MIDDAY_RUN_TITLE}`;
   }
   if (runHour > 14 && runHour <= 18) {
-    return RUN_TITLES.AFTERNOON_RUN_TITLE;
+    return `${run.name.includes('徒步') ? '午后徒步' : RUN_TITLES.AFTERNOON_RUN_TITLE}`;
   }
   if (runHour > 18 && runHour <= 21) {
-    return RUN_TITLES.EVENING_RUN_TITLE;
+    return `${run.name.includes('徒步') ? '傍晚徒步' : RUN_TITLES.EVENING_RUN_TITLE}`;
   }
-  return RUN_TITLES.NIGHT_RUN_TITLE;
+  return `${run.name.includes('徒步') ? '夜晚徒步' : RUN_TITLES.NIGHT_RUN_TITLE}`;
 };
 
 export interface IViewState {
